0db86e13fdc5f21171e57342a48b885004761338
Duncan Thomas <duncan.thomas@gmail.com>
2019-03-25 03:55:10 +0000

    New style blog is a go

---
+++ precache-manifest.9566c1f2d1218f8db2d729129c8d02c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a0fe92b078464fe9dd7",
+    "url": "/static/js/main.67e4b447.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e23ce1f0d9a2c761faca",
+    "url": "/static/js/2.ca0ab64b.chunk.js"
+  },
+  {
+    "revision": "a94138bce80dcad0ae7b595b95185cc4",
+    "url": "/index.html"
+  }
+];
